d9ad8049bdba9287fe66153d8c9d3f34de5b03b3
Harsh Mittal <harshmittal2210@gmail.com>
2024-11-11 23:26:48 +0530

    Update Status, Get Order List

--- customer_service/app/apis/v1/route_order.py
+++ customer_service/app/apis/v1/route_order.py
@@ -1,6 +1,6 @@
 # Built-in Dependencies
 from typing import Annotated, Dict, Any
-import os
+from uuid import UUID
 
 # Third-Party Dependencies
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -12,7 +12,10 @@ from sqlalchemy.sql import text
 # Local Dependencies
 from app.core.dependencies import CurrentUser, CurrentSuperUser
 from app.db.crud.crud_order import (
-    add_new_order
+    add_new_order,
+    update_order_status,
+    get_order_details,
+    get_order_list
 )
 from app.db.session import async_get_db
 from app.core.http_exceptions import (
@@ -27,7 +30,7 @@ from app.schemas.v1.schema_order import (
 )
 
 from app.db.models.v1.db_user import AccessLevel_Enum
-
+from app.db.models.v1.db_order import Order_Status_Enum
 from app.utils.paginated import (
     PaginatedListResponse,
     paginated_response,
@@ -46,3 +49,52 @@ async def create_order(
     current_user: CurrentUser,
 ) -> Any:
     return await add_new_order(user=current_user, order=order, db=db)
+
+@router.get("/list", response_model=PaginatedListResponse[OrderRead])
+async def order_list(
+    request: Request,
+    db: Annotated[AsyncSession, Depends(async_get_db)],
+    current_user: CurrentUser,
+    page: int = 1,
+    items_per_page: int = 10,
+):
+    return await get_order_list(
+        db=db,
+        items_per_page=items_per_page,
+        page=page,
+        user=current_user
+    )
+
+
+@router.get("/{order_id}", response_model=OrderRead)
+async def get_order(
+    request: Request,
+    order_id:UUID,
+    db: Annotated[AsyncSession, Depends(async_get_db)]
+):
+    return await get_order_details(db=db, order_id=order_id)
+
+
+
+@router.put("{order_id}", status_code=200)
+async def update_status(
+    request: Request,
+    order_id:UUID,
+    status: Order_Status_Enum,
+    db: Annotated[AsyncSession, Depends(async_get_db)],
+    restaurant_id: str | None = None,
+    customer_id: UUID | None = None,
+    driver_id: str | None = None,
+):
+    status =  await update_order_status(
+        customer_id=customer_id,
+        restaurant_id=restaurant_id,
+        driver_id=driver_id,
+        order_id=order_id,
+        status=status,
+        db=db
+    )
+
+    if status:
+        return {"status":"Updated"}
+    return {"status":"Not Updated, Check value sent"}
--- customer_service/app/db/crud/crud_order.py
+++ customer_service/app/db/crud/crud_order.py
@@ -18,6 +18,7 @@ from app.schemas.v1.schema_order import (
     OrderUpdateInternal,
     OrderDelete,
     OrderCreate,
+    OrderRead,
 
     OrderItemCreateInternal,
     OrderItemUpdate,
@@ -103,15 +104,56 @@ async def add_new_order(
             await crud_orderAddOn.create(db=db, object=add_on_c)
     return order_db
 
+async def get_order_details(
+    order_id: UUID,
+    db: AsyncSession,
+):
+    return await crud_order.get(db=db, id=order_id)
+
+async def get_order_list(
+    user: UserRead,
+    db: AsyncSession,
+    page: int = 1,
+    items_per_page: int = 10,
+):
+    order_data = await crud_order.get_multi(
+        db=db,
+        offset=compute_offset(page, items_per_page),
+        limit=items_per_page,
+        schema_to_select=OrderRead,
+        is_deleted=False,
+        customer_id=user.id
+    )
+    return paginated_response(
+        crud_data=order_data, page=page, items_per_page=items_per_page
+    )
 
 async def update_order_status(
-    user_id: UUID | None,
-    restaurant_id,
-    driver_id,
     order_id: UUID,
-    db: AsyncSession
+    status: Order_Status_Enum,
+    db: AsyncSession,
+    customer_id: UUID | None = None,
+    restaurant_id: str | None = None,
+    driver_id: str | None = None,
 ):
     order_db = await crud_order.get(db=db, id=order_id)
+    check_fag:bool = False
+    if customer_id is not None:
+        check_fag = True
+        if order_db["customer_id"] != customer_id:
+            raise ForbiddenException("Different Customer Trying to update order status")
 
-    # if user_id is not None:
-    #     if order_db.us
+    if restaurant_id is not None:
+        check_fag = True
+        if order_db["restaurant_id"] != restaurant_id:
+            raise ForbiddenException("Different Restaurant Trying to update order status")
+
+    if driver_id is not None:
+        check_fag = True
+        if order_db["driver_id"] != driver_id:
+            raise ForbiddenException("Different Driver Trying to update order status")
+    if check_fag is False:
+        raise ForbiddenException("Wrong Params Entered")
+    order_db["status"] = status
+    await crud_order.update(db=db, object=order_db, id=order_id)
+    return True
