4819eba46649d693f03a1abfa22c10a1712b4b2e
Harsh Mittal <harshmittal2210@gmail.com>
2024-11-16 14:58:29 +0530

    File Rename

--- restaurant_api/app/apis/base.py
+++ restaurant_api/app/apis/base.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
 from app.apis.v1 import route_login
-from app.apis.v1 import route_restaurant
+from app.apis.v1 import route_user
 
 api_router = APIRouter()
 
 api_router.include_router(route_login.router, prefix="/login")
-api_router.include_router(route_restaurant.router, prefix="/restaurants")
+api_router.include_router(route_user.router, prefix="/user")
 
